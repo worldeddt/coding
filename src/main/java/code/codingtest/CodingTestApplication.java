bc923515ace8403c111e68f6e712007a849a917b
worldeddt <ktest92@gmail.com>
2023-11-07 08:34:37 +0900

    DFS 테스트

--- src/main/java/code/codingtest/CodingTestApplication.java
+++ src/main/java/code/codingtest/CodingTestApplication.java
@@ -7,38 +7,37 @@ import java.util.Scanner;
 
 @SpringBootApplication
 public class CodingTestApplication {
-    static int n;
-    static int[] ch;
+    static String answer = "NO";
+    static int total, n = 0;
+    boolean flag = false;
 
     public static void main(String[] args) {
         SpringApplication.run(CodingTestApplication.class, args);
 
         CodingTestApplication codingTestApplication = new CodingTestApplication();
 
-        Integer i = 12;
-        Integer.toString(i).length();
-
         Scanner kb = new Scanner(System.in);
         n = kb.nextInt();
-        ch=new int[n+1];
-        codingTestApplication.DFS(1);
+        int[] arr = new int[n];
+        for (int i = 0; i< n; i++) {
+            arr[i] = kb.nextInt();
+            total += arr[i];
+        }
+        codingTestApplication.DFS(0,0, arr);
     }
 
-    public void DFS(int L) {
-        if (L == n+1) {
-            // 종착점
-            String tmp = "";
-            for (int i =1; i<=n; i++) {
-                if (ch[i] == 1) tmp += (i+" ");
+    // 1 3 5 6 7 9 10
+    public void DFS(int L, int sum, int[] arr) {
+        if (flag) return;
+        if (sum > total/2) return;
+        if (L == n) {
+            if ((total-sum) == sum) {
+                answer = "YES";
+                flag = true;
             }
-
-            if(tmp.length()>0) System.out.println(tmp);
-
         } else {
-            ch[L] = 1;
-            DFS(L+1);
-            ch[L] = 0;
-            DFS(L+1);
+            DFS(L+1, sum+arr[L], arr);
+            DFS(L+1, sum, arr);
         }
     }
 }
