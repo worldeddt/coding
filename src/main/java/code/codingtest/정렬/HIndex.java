c453e232fde6438d1cdc98ba7d3e4b7197cd12e3
worldeddt <ktest92@gmail.com>
2024-01-07 09:29:22 +0900

    HIndex 추가

---
+++ src/main/java/code/codingtest/정렬/HIndex.java
@@ -0,0 +1,15 @@
+package code.codingtest.정렬;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@Slf4j
+@SpringBootApplication
+public class HIndex {
+    public static void main(String[] args) {
+
+    }
+}
