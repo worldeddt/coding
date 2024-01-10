70a4db8ca32b82266709e25871d7257b93587242
worldeddt <ktest92@gmail.com>
2024-01-10 17:22:01 +0900

    feat: 여행경로 정답

--- src/main/java/code/codingtest/DFS/여행경로.java
+++ src/main/java/code/codingtest/DFS/여행경로.java
@@ -11,6 +11,7 @@ import java.util.List;
 @SpringBootApplication
 public class 여행경로 {
 
+    private List<String> answer;
     /**
      [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
      @result ["ICN", "JFK", "HND", "IAD"]
@@ -18,41 +19,46 @@ public class 여행경로 {
      @result ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"]
      */
     public static void main(String[] args) {
+        여행경로 ma = new 여행경로();
 //        String[][] tickets = {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
         String[][] tickets = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
-        boolean[] used = new boolean[tickets.length];
-        List<String> result = new ArrayList<>();
 
-        solve(tickets, used, result);
-        String[] array = result.toArray(new String[result.size()]);
+        String[] result2 = ma.solution(tickets);
 
-        for (String arr : array) {
-            log.info("result : {}", arr);
+        for (String re : result2) {
+            log.info("result : {}", re);
         }
 
-
-//        return result.toArray();
     }
 
-    static void solve(String[][] tickets, boolean[] used,
-                       List<String> result) {
+    public String[] solution(String[][] tickets) {
+        answer = new ArrayList<>();
+        boolean[] visited = new boolean[tickets.length];
 
-        for (int i = 0; i< tickets.length; i++) {
-            if (used[i]) continue;
+        // 출발 지점이 ICN이므로 "ICN"부터 시작
+        dfs("ICN", tickets, visited, 0);
 
-            for (int j = 0; j<tickets[i].length; j++) {
-                if (result.size() == 0) {
-                    result.add(tickets[i][0]);
-                    result.add(tickets[i][1]);
-                    used[i] = true;
-                    log.info("'tickets[i][j] : {} ", tickets[i][j]);
-                    solve(tickets, used, result);
-                } else if (result.get(result.size() - 1).equals(tickets[i][0])) {
-                    result.add(tickets[i][1]);
-                    used[i] = true;
-                    log.info("'tickets[i][j] : {} ", tickets[i][j]);
-                    solve(tickets, used, result);
-                }
+        // List를 배열로 변환
+        String[] result = new String[answer.size()];
+        answer.toArray(result);
+
+        return result;
+    }
+
+    private void dfs(String current, String[][] tickets, boolean[] visited, int depth) {
+        answer.add(current);
+
+        // 모든 항공권을 사용한 경우 종료
+        if (depth == tickets.length) {
+            return;
+        }
+
+        for (int i = 0; i < tickets.length; i++) {
+            // 사용하지 않은 항공권 중 현재 위치에서 출발하는 항공권을 찾음
+            if (!visited[i] && tickets[i][0].equals(current)) {
+                visited[i] = true;
+                dfs(tickets[i][1], tickets, visited, depth + 1);
+                visited[i] = false;  // backtracking
             }
         }
     }
